38ccceca243a69b8cd02c6d8b868becf6b71d27f
v3nk3y <nagireddyvenkatesh@gmail.com>
2019-10-21 09:56:57 -0400

    add docker files

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient, /* other http imports */ } from '@angular/common/http';
+// Import rxjs map operator
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-app';
+export class AppComponent implements OnInit {
+  title = 'app works!';
+
+  // Link to our api, pointing to localhost
+  API = 'http://localhost:3000';
+
+  // Declare empty list of people
+  people: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  // Angular 2 Life Cycle event when component has been initialized
+  ngOnInit() {
+    this.getAllPeople();
+  }
+
+  // Add one person to the API
+  addPerson(name, age) {
+    this.http.post(`${this.API}/users`, {name, age})
+      .subscribe((data: any) => {
+        this.getAllPeople();
+      }, (error: any) => {console.log(error); });
+  }
+
+  // Get all users from the API
+  getAllPeople() {
+    this.http.get(`${this.API}/users`)
+      .subscribe((people: any ) => {
+        console.log(people);
+        this.people = people;
+      }, (error: any) => {console.log(error); });
+  }
 }
